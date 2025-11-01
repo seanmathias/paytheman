39d631ee30042702ff0de74881fb62f6ad73a683
Sean Mathias <seanmathias@spartan.mountainmudder.com>
2025-11-01 15:41:39 -0700

    Inital implementation

---
+++ config.js
@@ -0,0 +1,6 @@
+// Configuration file for fitness dashboard
+// Keep this file private and do not commit to version control
+
+const SHEET_CONFIG = {
+    SHEET_URL: 'https://docs.google.com/spreadsheets/d/1QnFVeP_C-r163DN1pybzxoeICz0ueaVAnbNjQ6E7Vd0/edit'
+};
